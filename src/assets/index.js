e7f9d2a1e3bc68e1c3c04c3e7b59e0a1dc0f8489
efremovid <i.efremov.psk@yandex.ru>
2025-10-07 20:48:59 +0300

    1

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,24 @@
-const photos = [
-  "/src/assets/back/фото заведения/бирюлёво/1.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/2.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/3.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/4.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/5.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/6.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/7.jpg",
-  "/src/assets/back/фото заведения/бирюлёво/8.jpg",
-];
+// const photos = [
+//   "./src/assets/back/фото заведения/бирюлёво/1.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/2.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/3.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/4.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/5.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/6.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/7.jpg",
+//   "/src/assets/back/фото заведения/бирюлёво/8.jpg",
+// ];
+
+import img1 from "./back/фото заведения/бирюлёво/1.jpg";
+import img2 from "./back/фото заведения/бирюлёво/2.jpg";
+import img3 from "./back/фото заведения/бирюлёво/3.jpg";
+import img4 from "./back/фото заведения/бирюлёво/4.jpg";
+import img5 from "./back/фото заведения/бирюлёво/5.jpg";
+import img6 from "./back/фото заведения/бирюлёво/6.jpg";
+import img7 from "./back/фото заведения/бирюлёво/7.jpg";
+import img8 from "./back/фото заведения/бирюлёво/8.jpg";
+
+const photos = [img1, img2, img3, img4, img5, img6, img7, img8];
 
 export const menuBirulevo = [
   "/src/assets/back/меню бирюлёво/1.png",
